cbc9b8d30cacec2598ba4c21c4a8fe0d10e56592
andresncenturion <andresncenturion@gmail.com>
2017-11-08 22:13:21 -0300

    TP SPD
    Guarda password por mic

---
+++ TP_SPD.ino
@@ -0,0 +1,57 @@
+int noiseFloor = 0;
+int noiseRoof;
+int actualNoise;
+int startFlag = 0;
+int recFlag = 0;
+int wFlag = 0;
+int i = 0;
+unsigned long pass[3];
+unsigned long k0;
+unsigned long auxK;
+float tolerance = 1.15;
+
+void setup() 
+{
+  Serial.begin (9600);
+  pass[0]=0;
+  pass[1]=0;
+  pass[2]=0;
+}
+
+void loop() 
+{
+  if (startFlag == 0)
+  {
+  startFlag = 1;
+  noiseFloor = analogRead (A0);
+  noiseRoof = noiseFloor * tolerance;
+  }
+  else
+  {
+  actualNoise = analogRead (A0);
+  if (actualNoise > noiseRoof)
+    {
+      if (recFlag == 0)
+      {
+        recFlag = 1;
+        k0 = millis ();
+        delay (100);
+      }
+      else
+      {
+      auxK = millis ();
+      auxK = auxK - k0;
+      pass[i] = auxK;
+      i++;
+      delay (100);
+      }
+    }
+  }
+  if (pass[2] != 0 && wFlag == 0)
+  {
+  wFlag = 1;
+  Serial.println (pass[0]);
+  Serial.println (pass[1]);
+  Serial.println (pass[2]);
+  }
+}
